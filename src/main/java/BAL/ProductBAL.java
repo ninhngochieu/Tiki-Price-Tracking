1f4d4d23925ea665571bf6feb9e698f7637c16a5
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2020-10-29 10:29:58 +0700

    Update ProductBAL.java

--- src/main/java/BAL/ProductBAL.java
+++ src/main/java/BAL/ProductBAL.java
@@ -39,6 +39,17 @@ public class ProductBAL{
             return productDAL.getByIdName(params.get("idCategory"),params.get("key"));
         }
     }
+    public ArrayList<Object> getAllProductWithParam(HashMap<String, String> params, int per_page, int current_page) {
+        if(params.get("idCategory").equalsIgnoreCase("1")){
+            return productDAL.getByName(params.get("key"),per_page,current_page);
+        }else {
+            return productDAL.getByIdName(params.get("idCategory"),params.get("key"),per_page,current_page);
+        }
+    }
+
+    public int getTotal(HashMap<String, String> params) {
+        return productDAL.getTotalByName(params.get("key")).size();
+    }
 
 //    public Object getSuggestNameProduct(HashMap<String, String> params) {
 //        ArrayList<ProductDTO> list_product= instance.list_product;
